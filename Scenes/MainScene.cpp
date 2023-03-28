14593f8307835762cdae85ffaa0f89adfb7470b6
VasiliyDenisov <mr.v.m.denisov@mail.ru>
2023-03-28 10:50:39 +0300

    Remove trash, add button class

--- Scenes/MainScene.cpp
+++ Scenes/MainScene.cpp
@@ -2,31 +2,14 @@
 #include <SFML/System/Vector2.hpp>
 
 void MainScene:: drawScene() {
-    window.draw(play_button);
     button.draw();
 }
 
 bool MainScene:: logic() {
-    int btn_pos_x = play_button.getPosition().x, btn_pos_y = play_button.getPosition().y;
-    int mouse_pos_x = sf::Mouse::getPosition(window).x, mouse_pos_y = sf::Mouse::getPosition(window).y;
-
-    if (btn_pos_x <= mouse_pos_x && mouse_pos_x <= btn_pos_x + play_button.getSize().x &&
-        btn_pos_y <= mouse_pos_y && mouse_pos_y <= btn_pos_y + play_button.getSize().y) {
-        play_button.setFillColor(sf::Color::Blue);
-        if (sf:: Mouse::isButtonPressed(sf::Mouse::Left)) {
-            return true;
-        }
-    } else {
-        play_button.setFillColor(sf::Color::Red);
-    }
-    return false;
+    return  button.isPressed();
 }
 
 
 MainScene:: MainScene(int h, int w, sf::RenderWindow& win) : height(h), width(w), window(win), play_button(sf::Vector2f(0, 0)), button(win)  {
-    play_button.setSize(sf::Vector2f(height / 4.0f, width / 10.0f));
-    play_button.setPosition(height / 2.0f - play_button.getSize().x / 2, width / 2.0f - play_button.getSize().y / 2);
-    play_button.setFillColor(sf::Color::Cyan);
-
-    button.setUp(sf:: Vector2f(100, 100), sf:: Vector2f(100, 100), "HELLO");
+    button.setUp(sf:: Vector2f(100, 50), sf:: Vector2f(100, 50), "Hello");
 }
--- templates/Button.h
+++ templates/Button.h
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Vector2.hpp>
 #include <sstream>
@@ -8,29 +9,41 @@ public:
     void draw() {
         window.draw(rect);
         window.draw(text);
+        hover();
     }
 
     Button(sf::RenderWindow& win) : font(), window(win), rect() {}
 
-//    Button(sf::Vector2f& size, sf::Vector2f& pos, std:: string str, sf::RenderWindow& win) : font(), window(win), rect() {
-//        font.loadFromFile("../fonts/Hack-Regular.ttf");
-//        text = sf:: Text(str, font, 22);
-//        text.setPosition(pos);
-//        rect.setPosition(pos);
-//        rect.setScale(size);
-//    }
-
     void setUp(sf::Vector2f size, sf::Vector2f pos, std:: string str, std:: string Font = "fonts/Hack-Regular.ttf", int font_size=22) {
-        if (!font.loadFromFile(Font)) std:: cout << "QWEQWE";
+        if (!font.loadFromFile(Font)) std:: cout << "Font error";
         text = sf:: Text(str, font, font_size);
-        text.setPosition(pos);
-        rect.setPosition(pos);
         rect.setSize(size);
+        auto center = sf:: Vector2f(text.getGlobalBounds().width / 2.0f, text.getGlobalBounds().height / 1.2f);
+        auto localBounds = center + text.getPosition();
+        text.setOrigin(localBounds);
+        text.setPosition(sf::Vector2f{ window.getSize() / 2u });
+        rect.setOrigin(sf::Vector2f(rect.getGlobalBounds().width / 2.f, rect.getGlobalBounds().height / 2.0f));
+        rect.setPosition(text.getPosition());
         text.setFillColor(sf::Color(0,0,0));
         rect.setFillColor(sf::Color(255,255,255));
-    }  
+    }
+
+    bool isPressed() {
+        if (rect.getPosition().x <= sf::Mouse::getPosition().x && sf::Mouse::getPosition(window).x <= rect.getPosition().x + rect.getSize().x &&
+            rect.getPosition().y <= sf::Mouse::getPosition().y && sf::Mouse::getPosition(window).y<= rect.getPosition().y + rect.getSize().y) {
+            if (sf:: Mouse::isButtonPressed(sf::Mouse::Left)) return true;
+        }
+        return false;
+    }
 
 private:
+    void hover() {
+        if (rect.getPosition().x <= sf::Mouse::getPosition(window).x && sf::Mouse::getPosition(window).x <= rect.getPosition().x + rect.getSize().x &&
+            rect.getPosition().y <= sf::Mouse::getPosition(window).y && sf::Mouse::getPosition(window).y <= rect.getPosition().y + rect.getSize().y) {
+            rect.setFillColor(sf::Color::Blue);
+        } else rect.setFillColor(sf::Color::Red);
+    }
+
 
     sf::Text text;
     sf::Font font;
